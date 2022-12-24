ddd08827ada0e78016b475bedea557431bc0e07e
swittuthcoding <swittuthcoding@gmail.com>
2022-12-24 00:22:36 -0500

    updated styling

--- components/InputRecommendation.tsx
+++ components/InputRecommendation.tsx
@@ -1,7 +1,7 @@
 const InputRecommendation = ({ recommendation }) => {
   return (
     <>
-      <ul className='bg-slate-400 absolute'>
+      <ul className='bg-slate-400 absolute w-full right-0'>
         {recommendation.map((obj: { name: string; code: string }) => (
           <li key={crypto.randomUUID()}>{obj.name}</li>
         ))}
