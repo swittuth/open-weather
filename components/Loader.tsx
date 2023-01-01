f95b6656687ff76d0a43cbf19b30e9e3e7b56d94
swittuthcoding <swittuthcoding@gmail.com>
2022-12-31 21:47:35 -0500

    updated styling for loader section

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className='flex gap-1 items-center justify-center absolute w-full'>
+    <div className='flex gap-1 items-center justify-center absolute w-full p-2 bg-slate-50'>
       <span className='w-2 h-2 animate-appear_1st bg-slate-500 rounded-full'></span>
       <span className='w-2 h-2 animate-appear_2nd bg-slate-500 rounded-full'></span>
       <span className='w-2 h-2 animate-appear_3rd bg-slate-500 rounded-full'></span>
