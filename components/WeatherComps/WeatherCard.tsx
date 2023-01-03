8a4a645b1215e9e9feecc2f99c447d7ac3e59702
swittuthcoding <swittuthcoding@gmail.com>
2023-01-02 21:04:42 -0500

    created weathercard comp

--- components/WeatherComps/WeatherCard.tsx
+++ components/WeatherComps/WeatherCard.tsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import AppContext from '../context/state';
+
 const WeatherCard = () => {
+  const { currWeatherData } = useContext(AppContext);
+
   return (
     <div>
       <p>WeatherCard placeholder</p>
