918c11174819c1d553ad332a4872b1fc05b4f911
swittuthcoding <swittuthcoding@gmail.com>
2023-01-02 22:04:03 -0500

    obtain relevant information to display weather data

--- components/WeatherComps/WeatherCard.tsx
+++ components/WeatherComps/WeatherCard.tsx
@@ -1,21 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import AppContext from '../context/state';
 import ClearDaySvg from '../../public/weather_assets/clear-day.svg';
 import Image from 'next/image';
 
 const WeatherCard = () => {
   const { currWeatherData } = useContext(AppContext);
-  console.log('WeatherCard', currWeatherData);
+  const {
+    condition_description,
+    feels_like,
+    name,
+    time,
+    temp_min,
+    temp_max,
+    humidity,
+    temp,
+  } = currWeatherData;
+  // console.log('WeatherCard', currWeatherData);
+
+  useEffect(() => {
+    // need to be able to detect weather condition in order to display svg
+  }, [currWeatherData]);
 
   return (
     <div>
       <Image
         src='/weather_assets/mist.svg'
-        width='100'
-        height='100'
+        width='300'
+        height='300'
         alt='Current Weather Condition Svg'
       />
-      <p>WeatherCard placeholder</p>
     </div>
   );
 };
