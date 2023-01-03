b4692876673d190fca062a1e27842881b25dd9b9
swittuthcoding <swittuthcoding@gmail.com>
2023-01-02 22:26:32 -0500

    installed moment

--- components/WeatherComps/WeatherCard.tsx
+++ components/WeatherComps/WeatherCard.tsx
@@ -1,25 +1,37 @@
 import { useContext, useEffect } from 'react';
 import AppContext from '../context/state';
-import ClearDaySvg from '../../public/weather_assets/clear-day.svg';
 import Image from 'next/image';
 
 const WeatherCard = () => {
-  const { currWeatherData } = useContext(AppContext);
+  const { currWeatherData, imageWeather, setImageWeather } =
+    useContext(AppContext);
   const {
-    condition_description,
+    condition_description = '',
     feels_like,
     name,
-    time,
+    time = '',
     temp_min,
     temp_max,
     humidity,
     temp,
   } = currWeatherData;
-  // console.log('WeatherCard', currWeatherData);
 
   useEffect(() => {
     // need to be able to detect weather condition in order to display svg
-  }, [currWeatherData]);
+    const desc_arr = condition_description.split(' ');
+    const time_arr = (time + '').split(' ');
+    console.log(time_arr);
+    if (desc_arr.length === 1) {
+      const noun = desc_arr[0];
+      if (noun === 'rain') {
+      } else if (noun === 'snow') {
+      } else if (noun === 'mist') {
+      } else if (noun === 'thunderstorm') {
+      }
+    } else if (desc_arr.length === 2) {
+      const [adj, noun] = desc_arr;
+    }
+  }, [condition_description]);
 
   return (
     <div>
