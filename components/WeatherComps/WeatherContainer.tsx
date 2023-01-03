307587512a56bbc8b2081bb72e24824dbd5d52dd
swittuthcoding <swittuthcoding@gmail.com>
2023-01-02 21:51:17 -0500

    updated layout for the app

--- components/WeatherComps/WeatherContainer.tsx
+++ components/WeatherComps/WeatherContainer.tsx
@@ -3,7 +3,7 @@ import WeatherInfoCard from './WeatherInfoCard';
 
 const WeatherContainer = () => {
   return (
-    <div className='w-full h-full border-red-500 border-2'>
+    <div className='w-full h-full border-red-500 border-2 flex flex-col justify-center items-center'>
       <WeatherCard />
       <WeatherInfoCard />
     </div>
