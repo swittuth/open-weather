ae698d4fa0570d2a40cdec544345acf4fd6ad56d
swittuthcoding <swittuthcoding@gmail.com>
2023-01-04 16:42:22 -0500

    updated layout of the application

--- components/WeatherComps/WeatherContainer.tsx
+++ components/WeatherComps/WeatherContainer.tsx
@@ -1,9 +1,17 @@
 import WeatherCard from './WeatherCard';
 import WeatherInfoCard from './WeatherInfoCard';
+import { useContext } from 'react';
+import AppContext from '../context/state';
+import styles from './WeatherContainer.module.css';
 
 const WeatherContainer = () => {
+  const { currWeatherData } = useContext(AppContext);
+  const { name } = currWeatherData;
+
   return (
-    <div className='w-full h-full border-red-500 border-2 flex flex-col justify-center items-center'>
+    <div className={`w-full h-full ${styles.weatherContainer}`}>
+      <h1>Location Tags</h1>
+      <h1>{name}</h1>
       <WeatherCard />
       <WeatherInfoCard />
     </div>
